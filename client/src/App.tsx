e25149405baf582e0aed0884f573ee7d0bfae9e9
IMMORTAL <74589262+aloneplayzz@users.noreply.github.com>
2025-09-26 15:55:10 +0000

    feat: Add modern navigation bar similar to bre4d.vercel.app

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,6 +3,7 @@ import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
+import NavigationBar from "@/components/NavigationBar";
 import Portfolio from "@/pages/portfolio";
 import Blog from "@/pages/blog";
 import BlogPost from "@/pages/blog-post";
@@ -23,6 +24,7 @@ function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <TooltipProvider>
+        <NavigationBar />
         <Toaster />
         <Router />
       </TooltipProvider>
